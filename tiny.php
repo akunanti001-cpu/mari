e0aa508f7ca3180be687d06eda5b36bb365600a6
akunanti001-cpu <akun.anti001@gmail.com>
2026-04-29 05:01:45 +0700

    Update tiny.php

--- tiny.php
+++ tiny.php
@@ -357,19 +357,50 @@ if ($use_auth) {
                                 <form class="form-signin" action="" method="post" autocomplete="off">
                                     <div class="mb-3">
                                         <div class="brand">
-                                            <svg version="1.0" xmlns="http://www.w3.org/2000/svg" M1008 width="100%" height="80px" viewBox="0 0 238.000000 140.000000" aria-label="H3K Tiny File Manager">
-                                                <g transform="translate(0.000000,140.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
-                                                    <path d="M160 700 l0 -600 110 0 110 0 0 260 0 260 70 0 70 0 0 -260 0 -260 110 0 110 0 0 600 0 600 -110 0 -110 0 0 -260 0 -260 -70 0 -70 0 0 260 0 260 -110 0 -110 0 0 -600z" />
-                                                    <path fill="#003500" d="M1008 1227 l-108 -72 0 -117 0 -118 110 0 110 0 0 110 0 110 70 0 70 0 0 -180 0 -180 -125 0 c-69 0 -125 -3 -125 -6 0 -3 23 -39 52 -80 l52 -74 73 0 73 0 0 -185 0 -185 -70 0 -70 0 0 115 0 115 -110 0 -110 0 0 -190 0 -190 181 0 181 0 109 73 108 72 1 181 0 181 -69 48 -68 49 68 50 69 49 0 249 0 248 -182 -1 -183 0 -107 -72z" />
-                                                    <path d="M1640 700 l0 -600 110 0 110 0 0 208 0 208 35 34 35 34 35 -34 35 -34 0 -208 0 -208 110 0 110 0 0 212 0 213 -87 87 -88 88 88 88 87 87 0 213 0 212 -110 0 -110 0 0 -208 0 -208 -70 -69 -70 -69 0 277 0 277 -110 0 -110 0 0 -600z" />
-                                                </g>
-                                            </svg>
-                                        </div>
-                                        <div class="text-center">
-                                            <h1 class="card-title"><?php echo APP_TITLE; ?></h1>
+                                            <svg xmlns="http://www.w3.org/2000/svg"
+     width="100%" height="80px"
+     viewBox="0 0 320 140"
+     aria-label="BUG Glitch Logo">
+
+  <defs>
+    <style>
+      .main {
+        font-family: Arial Black, Impact, sans-serif;
+        font-size: 110px;
+        font-weight: 900;
+        fill: #000000;
+      }
+
+      .glitch-red {
+        fill: #ff004c;
+        opacity: 0.7;
+      }
+
+      .glitch-cyan {
+        fill: #00bcd4;
+        opacity: 0.7;
+      }
+    </style>
+  </defs>
+
+  <!-- background putih -->
+  <rect width="100%" height="100%" fill="#ffffff"/>
+
+  <!-- glitch layers -->
+  <text x="25" y="105" class="main glitch-cyan" transform="translate(-3,0)">BUG</text>
+  <text x="25" y="105" class="main glitch-red" transform="translate(3,0)">BUG</text>
+
+  <!-- main text -->
+  <text x="25" y="105" class="main">BUG</text>
+
+  <!-- glitch slices -->
+  <rect x="35" y="42" width="240" height="8" fill="#00bcd4" opacity="0.12"/>
+  <rect x="60" y="70" width="180" height="6" fill="#ff004c" opacity="0.12"/>
+  <rect x="95" y="96" width="200" height="5" fill="#00bcd4" opacity="0.10"/>
+
+</svg>
                                         </div>
                                     </div>
-                                    <hr />
                                     <div class="mb-3">
                                         <label for="fm_usr" class="pb-2"><?php echo lng('Username'); ?></label>
                                         <input type="text" class="form-control" id="fm_usr" name="fm_usr" required autofocus>
@@ -393,8 +424,6 @@ if ($use_auth) {
                             </div>
                         </div>
                         <div class="footer text-center">
-                            &mdash;&mdash; &copy;
-                            <a href="https://tinyfilemanager.github.io/" target="_blank" class="text-decoration-none text-muted" data-version="<?php echo VERSION; ?>">CCP Programmers</a> &mdash;&mdash;
                         </div>
                     </div>
                 </div>
